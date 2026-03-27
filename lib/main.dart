c363d050543851ac1c3c050a7a1e91e7e4bb57b2
thelazybastard <256585194+thelazybastard@users.noreply.github.com>
2026-03-27 18:52:40 +0700

    got basic skeleton up and running

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sub_rosa/ui/translate.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sub_rosa/ui/settings.dart';
+
 
 void main() {
   runApp(Rosa());
@@ -10,8 +13,75 @@ class Rosa extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Translate(),
+    return MaterialApp.router(
+      routerConfig: _router,
+    );
+  }
+}
+
+final GoRouter _router = GoRouter(
+  initialLocation: '/translate',
+  routes: [
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return MainScaffold(navigationShell: navigationShell);
+      },
+      branches: [
+        
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/translate',
+              builder: (context, state) => const Center(child: Translate()),
+            ),
+          ],
+        ),
+       
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/history',
+              builder: (context, state) => const Center(child: Settings()),
+            ),
+          ],
+        ),
+        
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/settings',
+              builder: (context, state) => const Center(child: Text('Settings Screen')),
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
+
+class MainScaffold extends StatelessWidget {
+  const MainScaffold({required this.navigationShell, super.key});
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell, 
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: navigationShell.currentIndex,
+        onDestinationSelected: (int index) {
+          navigationShell.goBranch(
+            index,
+            initialLocation: index == navigationShell.currentIndex,
+          );
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.translate), label: 'Translate'),
+          NavigationDestination(icon: Icon(Icons.history), label: 'History'),
+          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
     );
   }
 }
--- lib/ui/translate.dart
+++ lib/ui/translate.dart
@@ -10,6 +10,6 @@ class Translate extends StatefulWidget {
 class _TranslateState extends State<Translate> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Text("HEllow");
   }
 }
